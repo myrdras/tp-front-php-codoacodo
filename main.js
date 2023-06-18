89cb4b65c6639dc71a017119ed4e456120668c8b
unknown <mgespindola93@gmail.com>
2023-06-18 01:21:04 -0300

    Buy-tickets desaparece sino estoy en la url

--- main.js
+++ main.js
@@ -62,25 +62,3 @@ function getTotal() {
 function borrar() {
    document.getElementById("total").innerText = 0;
 }
-/*
-// Hide if it's not on display
-let callback = (entries) => {
-   console.log("1");
-   if (!estoyEnBuyTickets) {
-      console.log("2");
-      entries.forEach(entry => {
-         console.log("3");
-         if (!entry.isIntersecting) {
-            console.log("4");
-            sectionTickets.classList.add("d-none");
-         }
-         //entry.target.style.backgroundColor = entry.isIntersecting ? 'green' : 'red';
-      })
-   }
-}
-let observer = new IntersectionObserver(callback, {
-   threshold: [0.1] // If 10% of the element is in the screen, we count it!
-   // Can change the thresholds based on your needs. The default is 0 - it'll run only when the element first comes into view
-});
-
-if (sectionTickets) observer.observe(sectionTickets);*/
